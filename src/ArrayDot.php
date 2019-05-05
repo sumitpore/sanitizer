c885f66f3c2f008d50979d54d4eafad5845e90d4
Sumit Pore <sumit.pore@gmail.com>
2019-05-05 20:31:41 +0530

    Add support for retrieving all possible keys from wildcards in dot-key

---
+++ src/ArrayDot.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Rees\Sanitizer;
+
+class ArrayDot
+{
+    /**
+     * Checks whether the array is multidimensional or not
+     *
+     * @param array $array Array to be checked
+     * @return boolean
+     */
+    public static function isMultiDimensionalArray(array $array): bool
+    {
+        rsort($array);
+        return isset($array[0]) && is_array($array[0]);
+    }
+
+
+    /**
+     * Collapse an array of arrays into a single array.
+     * Adapted from: https://github.com/illuminate/support/blob/5.8/Arr.php#L47
+     * @param  array  $array
+     * @return array
+     */
+    public static function collapse($array): array
+    {
+        if(!static::isMultiDimensionalArray($array)){
+            return $array;
+        }
+
+        $results = [];
+        foreach ($array as $values) {
+            if (! is_array($values)) {
+                continue;
+            }
+            $results = array_merge($results, $values);
+        }
+
+        return $results;
+    }
+
+    /**
+     * Resolves wild card key to possible keys and returns array of those possible keys
+     *
+     * @param array $array Array in which keys to be found out
+     * @param string $key Wildcard Key passed in "dot" format
+     * @param string $prefix Prefix to be applied on found keys
+     * @return array
+     */
+    public static function resolveWildcardKey(array $array, $key = null, string $prefix = ''): array {
+        $prefixSegments = [];
+
+        // If no key is passed or * is the only string passed to the key, then 
+        // convert array to dot array & return keys from found in dot array
+        if (empty($key) || (is_string($key) && '*' == $key)) {
+            return array_map(
+                function($_key) use($prefix) {
+                    return !empty($prefix) ? $prefix . '.' . $_key : $_key;
+                },
+                array_keys(array_dot($array))
+            );
+        }
+
+        // If key ends with '.', do not allow to proceed.
+        if(is_string($key) && substr($key, -1) == '.'){
+            throw new \InvalidArgumentException('Key can not end with `.`');
+        }
+
+        // If key does not contain '*', then check if the passed key exists in the 
+        // array or not. If it exists, then return the key, else return blank []
+        if(is_string($key) && strpos($key, '*') === false){
+            $finalPrefix = !empty( $prefix ) ? $prefix . '.' . $key : $key;
+            return array_has($array, $finalPrefix) ? [$key] : [];
+        }
+
+        $key = is_array($key) ? $key : explode('.', $key);
+
+        while (! is_null($segment = array_shift($key))) {
+
+            if ($segment === '*') {
+                $result = [];
+               
+                foreach ($array as $internalKey => $item) {
+
+                    if(!is_array($item)) {
+                        $result[] = implode(
+                            '.', 
+                            array_filter(array_merge([$prefix], $prefixSegments, [$internalKey]), 'strlen')
+                        );
+                        continue;
+                    }
+
+                    $result[] = static::resolveWildcardKey(
+                        $item, 
+                        $key, 
+                        implode(
+                            '.', 
+                            array_filter(array_merge([$prefix],$prefixSegments, [$internalKey]), 'strlen')
+                        )
+                    );
+                }
+
+                return static::collapse($result);
+            }
+
+            // Invalid key is passed
+            if (is_array($array) && ! array_key_exists($segment, $array)) {
+                return [];
+            }
+
+            if (is_array($array) && array_key_exists($segment, $array)) {
+                $array = $array[$segment];
+                $prefixSegments[] = $segment;
+            }
+
+        }
+
+        $combinedPrefixSegments = implode('.', $prefixSegments);
+        return !empty($prefix) ? [$prefix . '.' . $combinedPrefixSegments] : [$combinedPrefixSegments];
+   
+    }
+
+}
---
+++ tests/ArrayDotTest.php
@@ -0,0 +1,136 @@
+<?php
+use Rees\Sanitizer\ArrayDot;
+
+class ArrayDotTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testThatIsMultiDimensionalArrayMethodCanCheckIfArrayIsMultiDimensional(){
+        $array = [
+            'foo' => [
+                'bar' => [
+                    'baz' => 123,
+                    'qux' => 456
+                ]
+            ],
+            'comments' => [
+                ['id' => 1, 'text' => 'foo'],
+                ['id' => 2, 'text' => 'bar'],
+                ['id' => 3, 'text' => 'baz'],
+            ]
+        ];
+
+        $this->assertTrue(ArrayDot::isMultiDimensionalArray($array));
+
+        $array =[
+            'foo',
+            'bar',
+            'baz'
+        ];
+
+        $this->assertFalse(ArrayDot::isMultiDimensionalArray($array));
+    }
+
+    public function testThatCollapseMethodCanConvertCollectionOfArraysToSingleFlatArray(){
+        $array = [
+            [1, 2, 3], 
+            [4, 5, 6], 
+            [7, 8, 9]
+        ];
+
+        $this->assertEquals(ArrayDot::collapse($array), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+
+    public function testThatResolveWildCardKeyThrowsExceptionIfKeyEndsWithDot(){
+        $array = [
+            'users' => [
+                'subscribers' => [
+                    'sumit' => [
+                        'id' => '12345',
+                        'name' => 'Sumit'
+                    ],
+                    'tony' => [
+                        'id' => '780643'
+                    ],
+                    'steve' => [
+                        'id' => '532678'
+                    ]
+                ]
+            ]
+        ];
+        $this->setExpectedException(InvalidArgumentException::class);
+        ArrayDot::resolveWildcardKey($array, 'a.id.');
+    }
+
+    public function testThatResolveWildCardKeyCanFindAllPossibleKeys(){
+        
+        $array = [
+            'users' => [
+                'subscribers' => [
+                    'sumit' => [
+                        'id' => '12345',
+                        'name' => 'Sumit'
+                    ],
+                    'tony' => [
+                        'id' => '780643',
+                    ],
+                    'steve' => [
+                        'id' => '532678'
+                    ]
+                ]
+            ]
+        ];
+
+        //Passing no key returns array of all dot keys
+        $this->assertEquals(ArrayDot::resolveWildcardKey($array), [
+            'users.subscribers.sumit.id',
+            'users.subscribers.sumit.name',
+            'users.subscribers.tony.id',
+            'users.subscribers.steve.id'
+        ]);
+
+        // Passing no key is similar to passing only *
+        $this->assertEquals(ArrayDot::resolveWildcardKey($array), ArrayDot::resolveWildcardKey($array, '*'));
+
+        // Passing a non-wildcard key will return the key as it is if it is a valid key
+        $this->assertEquals(ArrayDot::resolveWildcardKey($array, 'users.subscribers'), [
+            'users.subscribers'
+        ]);
+
+        // Passing invalid keys returns blank array
+        $this->assertEquals(ArrayDot::resolveWildcardKey($array, 'users.b.c'), []);
+
+        // Each star represents one level of hierarchy. `id` key is present at 3rd level hierarchy
+        $this->assertEquals(ArrayDot::resolveWildcardKey($array, 'users.*.id'), []);
+
+        $this->assertEquals(ArrayDot::resolveWildcardKey($array, 'users.*.*.id'), [
+            'users.subscribers.sumit.id',
+            'users.subscribers.tony.id',
+            'users.subscribers.steve.id'
+        ]);
+
+        $this->assertEquals(ArrayDot::resolveWildcardKey($array, 'users.subscribers.*'), [
+            'users.subscribers.sumit.id',
+            'users.subscribers.sumit.name',
+            'users.subscribers.tony.id',
+            'users.subscribers.steve.id'
+        ]);
+
+        $this->assertEquals(ArrayDot::resolveWildcardKey($array, 'users.subscribers.*.id'), [
+            'users.subscribers.sumit.id',
+            'users.subscribers.tony.id',
+            'users.subscribers.steve.id'
+        ]);
+
+
+        $this->assertEquals(ArrayDot::resolveWildcardKey($array, 'users.*.sumit.*'), [
+            'users.subscribers.sumit.id',
+            'users.subscribers.sumit.name'
+        ]);
+
+        $this->assertEquals(ArrayDot::resolveWildcardKey($array, '*.*.sumit'), [
+            'users.subscribers.sumit',
+        ]);
+
+    }
+
+}
